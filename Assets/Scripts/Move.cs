36753c3df083466f2a78ea0176df4c9160c2f21d
SHOW\shoei <shoeish0326@gmail.com>
2023-10-11 16:31:37 +0900

    レベル２完了

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -30,6 +30,10 @@ public class Move : MonoBehaviour
         _destination[2] = _target.transform.position.z;
         if (_myTransform.position.x == _destination[0] && _myTransform.position.y == _destination[1] && _myTransform.position.z == _destination[2])
         {
+            _moving = false;
+        }
+        else
+        {
             _moving = true;
         }
 
@@ -41,16 +45,38 @@ public class Move : MonoBehaviour
         float _rootX = _destination[0] - _myTransform.position.x;
         float _rootY = _destination[1] - _myTransform.position.y;
         float _rootZ = _destination[2] - _myTransform.position.z;
-        Vector3 _root = new Vector3(_rootX*_accerateMagnification, _rootY * _accerateMagnification, _rootZ * _accerateMagnification);
-        if(_root == Vector3.zero)
+        Vector3 _root = new Vector3(_rootX, _rootY , _rootZ);
+        _root /= _root.magnitude;
+        //if(_root == Vector3.zero)
+        //{
+        //    _moving = false;
+        //    _rb.velocity = Vector3.zero;
+        //}
+        //if(_rootZ == 0)
+        //{
+        //    _moving = false;
+        //    _rb.velocity = Vector3.zero;
+        //}
+        if (_moving)
         {
-            _moving = false;
-        }
-        if(_moving)
-        {
-            _rb.AddForce(_root, ForceMode.Impulse);
+            _rb.velocity = _root*_accerateMagnification;
+            //_rb.AddForce(_root, ForceMode.Acceleration);
             Debug.Log("óÕÇÕê≥ÇµÇ≠â¡Ç¶ÇÁÇÍÇƒÇ¢Ç‹Ç∑");
         }
+        else
+        {
+
+        }
         Debug.Log($"_moving:{_moving}");
+        Debug.Log($"_root:{_root}");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Goal"))
+        {
+            _moving = false;
+            _rb.velocity = Vector3.zero;
+        }
     }
 }
